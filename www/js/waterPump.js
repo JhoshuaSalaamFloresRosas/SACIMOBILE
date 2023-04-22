192cabd4a1a58499a0cd95f9fc1ab45d776f73b8
Shalanzila <122801649+JhoshuaSalaamFloresRosas@users.noreply.github.com>
2023-04-21 21:24:05 -0600

    Update waterPump.js

--- www/js/waterPump.js
+++ www/js/waterPump.js
@@ -24,10 +24,10 @@ console.log('checkAutomatico' + automatico.checked);
 manual.addEventListener('click', function () {
   if (manual.checked) {
     console.log('El elemento no está marcado');
-
-    $.ajax({
-      url: "http://192.168.1.195:5000/apagar_rele",
-      type: "GET",
+   
+   $.ajax({
+      url: " https://f8ea-2806-10a6-15-7eab-b987-943d-2e97-d7b8.ngrok-free.app/apagar_bomba",
+      type: "POST",
       success: function (response) {
         console.log(response);
       },
@@ -35,12 +35,19 @@ manual.addEventListener('click', function () {
         console.error(error); 
       }
     });
+    setTimeout(function() {
+      manual.disabled = false;
+      document.querySelector(".manual").style.opacity = 1;
+    }, 3000);
+    // Deshabilita el checkbox temporalmente
+    manual.disabled = true;
+    document.querySelector(".manual").style.opacity = 0.5;
 
   } else {
     console.log('El elemento está marcado');
     $.ajax({
-      url: "http://192.168.1.195:5000/encender_rele",
-      type: "GET",
+      url: " https://f8ea-2806-10a6-15-7eab-b987-943d-2e97-d7b8.ngrok-free.app/encender_bomba",
+      type: "POST",
       success: function (response) {
         console.log(response);
       },
@@ -48,6 +55,12 @@ manual.addEventListener('click', function () {
         console.error(error);
       }
     });
-
+    setTimeout(function() {
+      manual.disabled = false;
+      document.querySelector(".manual").style.opacity = 1;
+    }, 3000);
+    // Deshabilita el checkbox temporalmente
+    manual.disabled = true;
+    document.querySelector(".manual").style.opacity = 0.5;
   }
 });
